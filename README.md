4647b71105923be6e250f828f6543652c526a440
Tobias Hauth <tobias@swiftype.com>
2013-01-28 18:26:16 -0800

    simple search app using the Swiftype Android library

---
+++ README.md
@@ -0,0 +1,4 @@
+SwiftypeAndroidExample
+======================
+
+For details please have a look at [SwiftypeAndroid](https://github.com/swiftype/SwiftypeAndroid)
---
+++ src/com/example/mysitesearch/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.mysitesearch;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Context;
+import android.view.Menu;
+import android.widget.SearchView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        
+     // Associate searchable configuration with the SearchView
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.menu_search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        
+        return true;
+    }
+    
+}
